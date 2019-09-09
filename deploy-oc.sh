52f817a886aa9219f1941a6e5293890ae85617c1
Luis <luisegr149@gmail.com>
2019-09-09 11:43:28 -0500

    Fix image name

--- deploy-oc.sh
+++ deploy-oc.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 npm run build
-docker build -t api-gateway .
+docker build -t codi-api-gateway .
 
-docker tag api-gateway nexus-0.dev.openshift.multivaloresgf.local:18444/codi/api-gateway:latest
-docker push nexus-0.dev.openshift.multivaloresgf.local:18444/codi/api-gateway:latest
+docker tag codi-api-gateway nexus-0.dev.openshift.multivaloresgf.local:18444/codi/codi-api-gateway:dev
+docker push nexus-0.dev.openshift.multivaloresgf.local:18444/codi/codi-api-gateway:dev
 
 oc project codi
 
-oc delete all --selector app=codi-api-gateway
+oc delete all --selector app=codi-codi-api-gateway
 
 oc new-app --name=codi-api-gateway \
---docker-image=nexus-0.dev.openshift.multivaloresgf.local:18444/codi/api-gateway:latest \
+--docker-image=nexus-0.dev.openshift.multivaloresgf.local:18444/codi/codi-api-gateway:dev \
 -e MONGO_COLLECTION=rutasDev -e MONGO_DB=gateway
 
 oc env dc/codi-api-gateway --from=configmap/conexion-mongo
